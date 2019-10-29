5e3a8a38bbea2268b920bc3fd77bc81ce101bbdf
Madison Adiao <56092617+wowitsmadiao@users.noreply.github.com>
2019-10-29 12:49:31 -0700

    Lesson 1.3 completed
    All videos are complete.

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,6 +6,7 @@ public class FollowPlayer : MonoBehaviour
 {
 
     public GameObject player;
+    private Vector3 offset = new Vector3(0,5,-7);
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,6 @@ public class FollowPlayer : MonoBehaviour
     void Update()
     {
         // Offset the camera behind the player by adding to the player's position
-        transform.position = player.transform.position + new Vector3(0, 5, -7);
+        transform.position = player.transform.position + offset;
     }
 }
